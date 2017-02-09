9c06629cb3b79e232b3a0c1bb87c38cadb7cacca
GreenNovaStar <brianyip5@gmail.com>
2017-02-09 14:06:55 -0800

    Started to work on Shape and Size
    Working on Shape and Size, however shape and size is not yet final.
    (Code not yet finished)

--- Main.java
+++ Main.java
@@ -3,11 +3,42 @@ import java.util.Scanner;
 
 public class Main{
 
+  //Symbol Method (Start)
+  public static char symbolm(int a,char b){
+    if(a == 1){
+      b = '!';
+    }else if(a == 2){
+      b = '@';
+    }else if(a == 3){
+      b = '#';
+    }else if(a == 4){
+      b = '$';
+    }else if(a == 5){
+      b = '%';
+    }else if(a == 6){
+      b = '^';
+    }else if(a == 7){
+      b = '&';
+    }else if(a == 8){
+      b = '*';
+    }else if(a == 9){
+      b = '(';
+    }else if(a == 0){
+      b = ')';
+    }
+  return b;
+  }
+  //Symbol Method (End)
+
+  //drawShape Method (Start)
+  //public static String drawShape()
+
+
+
   public static void main(String[] args) {
 
     System.out.println("Hello and welcome to shape generator.");
     Scanner in = new Scanner(System.in);
-
 
     //Symbol
     System.out.println("Choose a symbol.");
@@ -22,31 +53,21 @@ public class Main{
     System.out.println("9 is for (");
     System.out.println("0 is for )");
     int number = in.nextInt();
-    char symbol;
+    char symbol = 'a';
+    symbol = symbolm(number,symbol);
+    System.out.println("You have chosen " + symbol);
 
-    if(number == 1){
-      symbol = '!';
-    }else if(number == 2){
-      symbol = '@';
-    }else if(number == 3){
-      symbol = '#';
-    }else if(number == 4){
-      symbol = '$';
-    }else if(number == 5){
-      symbol = '%';
-    }else if(number == 6){
-      symbol = '^';
-    }else if(number == 7){
-      symbol = '&';
-    }else if(number == 8){
-      symbol = '*';
-    }else if(number == 9){
-      symbol = '(';
-    }else if(number == 0){
-      symbol = ')';
-    }
+    //Size
+    System.out.println("Enter the size of for the shape.");
+    int size = in.nextInt();
+
 
     //Shape
+    System.out.println("Choose a shape to draw.");
+    System.out.println("Currently in stock is:");
+    System.out.println("Square");
+    String Shape = in.next();
+    //Shape=drawShape(Shape);
 
 
   }
---
+++ Shape.java
@@ -0,0 +1,19 @@
+public class Shape{
+
+  private String shape = new String();
+
+  Shape(String s){
+    shape = s;
+
+  }
+
+  public void setSize(int s){
+    //this.size = s;
+  }
+
+/*
+  public int getSize(){
+    //return this.size;
+  }
+*/
+}
